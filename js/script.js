f1a76778d5d7f74e1c472999b38df3933268349f
mrcordova <46950394+mrcordova@users.noreply.github.com>
2024-06-15 21:22:42 -0700

    add input mask to display credit card spaces

--- js/script.js
+++ js/script.js
@@ -7,4 +7,14 @@ const {
   unformatCreditCard,
 } = cleaveZen;
 
-// console.log(cleaveZen);
+const input = document.getElementById("card-num");
+input.addEventListener(
+  "input",
+  () => (input.value = formatNumber(input.value.replaceAll(" ", "")))
+);
+
+const formatNumber = (number) =>
+  number.split("").reduce((seed, next, index) => {
+    if (index !== 0 && !(index % 4)) seed += " ";
+    return seed + next;
+  }, "");
