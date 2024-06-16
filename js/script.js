765761d5122be8ffe83ffb9492eefcee395e5253
mrcordova <46950394+mrcordova@users.noreply.github.com>
2024-06-15 20:49:14 -0700

    add autocomplete

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const cleaveZen = window.cleaveZen;
+const {
+  formatCreditCard,
+  getCreditCardType,
+  registerCursorTracker,
+  DefaultCreditCardDelimiter,
+  unformatCreditCard,
+} = cleaveZen;
+
+// console.log(cleaveZen);
