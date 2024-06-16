290437c8efc18a3acec22fcfa5a20cb95dd8b154
mrcordova <46950394+mrcordova@users.noreply.github.com>
2024-06-15 22:15:18 -0700

    WIP : MM/YY code

--- js/script.js
+++ js/script.js
@@ -8,6 +8,14 @@ const {
 } = cleaveZen;
 
 const input = document.getElementById("card-num");
+const expMInput = document.getElementById("exp-date-m");
+const expYInput = document.getElementById("exp-date-y");
+
+const formatNumber = (number) =>
+  number.split("").reduce((seed, next, index) => {
+    if (index !== 0 && !(index % 4)) seed += " ";
+    return seed + next;
+  }, "");
 
 input.addEventListener(
   "input",
@@ -15,14 +23,23 @@ input.addEventListener(
 );
 input.addEventListener("input", (e) => {
   const displayNum = document.getElementById("front-card-num");
-  console.log(displayNum.textContent.slice(0, input.value.length));
+  //   console.log(displayNum.textContent.slice(0, input.value.length));
   displayNum.textContent = `${
     input.value + displayNum.textContent.slice(input.value.length)
   }`;
 });
 
-const formatNumber = (number) =>
-  number.split("").reduce((seed, next, index) => {
-    if (index !== 0 && !(index % 4)) seed += " ";
-    return seed + next;
-  }, "");
+expMInput.addEventListener("input", (e) => {
+  const displayMon = document.getElementById("front-card-date");
+  console.log(expYInput.value || "00");
+  displayMon.textContent = `${
+    (expMInput.value || "00") +
+    displayMon.textContent.slice(0, Math.min(expMInput.value.length, 2))
+  }/${expYInput.value || "00"}`;
+});
+expYInput.addEventListener("input", (e) => {
+  const displayMon = document.getElementById("front-card-date");
+  displayMon.textContent = `${expMInput.value || "00"}/${
+    expYInput.value + displayMon.textContent.slice(4)
+  }`;
+});
